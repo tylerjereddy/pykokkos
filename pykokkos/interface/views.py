6b4105358d5ff671d5ec7400e443c5784ac700ab
Tyler Reddy <tyler.je.reddy@gmail.com>
2023-02-17 15:17:25 -0700

    ENH: Subview __eq__ improvement
    * bring the `__eq__` implementation in `Subview`
    up to par with `__eq__` in `View`, for `flip` testing
    purposes

--- pykokkos/interface/views.py
+++ pykokkos/interface/views.py
@@ -520,11 +520,42 @@ class Subview(ViewType):
         return base_view
 
     def __eq__(self, other):
-        if isinstance(other, View):
-            if len(self.data) == 0 and len(other.data) == 0:
-                return True
-            result_of_eq = self.data == other.data
-            return result_of_eq
+        from pykokkos.lib.ufuncs import equal
+        if isinstance(other, float):
+            new_other = pk.View((), dtype=pk.double)
+            new_other[:] = other
+        elif isinstance(other, bool):
+            new_other = pk.View((), dtype=pk.bool)
+            new_other[:] = other
+        elif isinstance(other, int):
+            if self.ndim == 0:
+                ret = pk.View((), dtype=pk.bool)
+                ret[:] = int(self) == other
+                return ret
+            if 0 <= other <= 255:
+                other_dtype = pk.uint8
+            elif 0 <= other <= 65535:
+                other_dtype = pk.uint16
+            elif 0 <= other <= 4294967295:
+                other_dtype = pk.uint32
+            elif 0 <= other <= 18446744073709551615:
+                other_dtype = pk.uint64
+            elif -128 <= other <= 127:
+                other_dtype = pk.int8
+            elif -32768 <= other <= 32767:
+                other_dtype = pk.int16
+            elif -2147483648 <= other <= 2147483647:
+                other_dtype = pk.int32
+            elif -9223372036854775808 <= other <= 9223372036854775807:
+                other_dtype = pk.int64
+            new_other = pk.View((), dtype=other_dtype)
+            new_other[:] = other
+        elif isinstance(other, pk.Subview):
+            new_other = other
+        else:
+            print("type(other):", type(other))
+            raise ValueError("unexpected types!")
+        return equal(self, new_other)
 
 
     def __add__(self, other):
